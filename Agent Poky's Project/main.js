b71acd2620714a3e74a4ad861bf5f71994e8321e
AgentPoky <roleypolly@gmail.com>
2024-11-09 11:36:12 -0600

    Add files via upload

---
+++ Agent Poky's Project/main.js
@@ -0,0 +1,13 @@
+function redirect(){
+    window.location.href = "https://www.youtube.com/@%E3%83%84agentpoky";
+}
+
+function current(){alert("I'm making two edits Right now: Goodbye Chapter 5 and Chapter 2 Remix");}
+
+function recentShort(){
+    window.location.href = "https://www.youtube.com/shorts/s2ahDmV-od8";
+}
+
+function recentVideo(){
+    window.location.href = "https://www.youtube.com/watch?v=12xfi0BjZnM&t=67s";
+}
